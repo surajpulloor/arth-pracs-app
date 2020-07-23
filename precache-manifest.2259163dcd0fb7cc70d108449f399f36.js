80e39b02d7be5a013a4dd5b4cd9bf6bb9518f2c5
Suraj Pulloor <thegamer600@gmail.com>
2020-07-24 02:07:33 +0530

    Updates

---
+++ precache-manifest.2259163dcd0fb7cc70d108449f399f36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf3b2a14bcb39d6df49a524faeff044",
+    "url": "/arth-pracs-app/index.html"
+  },
+  {
+    "revision": "48162c479de25c62818f",
+    "url": "/arth-pracs-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "48162c479de25c62818f",
+    "url": "/arth-pracs-app/static/js/2.3f36de1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/arth-pracs-app/static/js/2.3f36de1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9bb1f0dff7b66164401",
+    "url": "/arth-pracs-app/static/js/main.7c442b86.chunk.js"
+  },
+  {
+    "revision": "9ab191add9213d53b712",
+    "url": "/arth-pracs-app/static/js/runtime-main.693a094b.js"
+  }
+]);
